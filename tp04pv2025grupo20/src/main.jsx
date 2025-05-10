e8451c08b62f89c9b0e4c0e14e560097db357fe0
valentina <valentinamdrgl@gmail.com>
2025-05-10 20:11:36 -0300

    ValentinaMadrigal-agrego producto

--- tp04pv2025grupo20/src/main.jsx
+++ tp04pv2025grupo20/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import App2 from './App2.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App2 /> 
+  </React.StrictMode>,
 )
