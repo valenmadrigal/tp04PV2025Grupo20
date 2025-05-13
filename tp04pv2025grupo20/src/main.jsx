afb9434f107d6c5020ae897b4769230e4233c94f
Franco <francoarcibia423@gmail.com>
2025-05-13 11:30:37 -0300

    Agregamiento de ProductItem "Franco Arcibia"

--- tp04pv2025grupo20/src/main.jsx
+++ tp04pv2025grupo20/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import App2 from './App2.jsx' 
+import App2 from './assets/components/Producto'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
